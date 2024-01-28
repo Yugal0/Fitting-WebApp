b821601f18dcd4d000a5ff87e259080ff424e582
Yugal0 <122299659+Yugal0@users.noreply.github.com>
2024-01-28 06:06:05 +0530

    latest

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html', message='Hello, World!')
 
+@app.route('/page1')
+def date_11():
+    return render_template("index.html",message="GGs")
+
 if __name__ == '__main__':
     app.run(debug=True)
